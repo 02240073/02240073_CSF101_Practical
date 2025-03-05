159c26c3b30eabba7866927fcf7fc6d689cfa88e
02240073 <02240073.cst@rub.edu.bt>
2025-03-05 15:22:05 -0700

    My commit mesage

---
+++ range_practical.py
@@ -0,0 +1,20 @@
+programatCST = ["EE", "ICE", "WRE", "SWE", "ECE", "CC", "ME", "IT", "ARCH", "GeO"]
+ 
+arraylength = len(programatCST)
+
+for index in range(arraylength):
+    print(programatCST[index].lower())
+
+for element in programatCST:
+    print(element)
+
+arrayLen = len(programatCST)
+index = 0
+while index < arrayLen:
+    print(programatCST[index])
+    index = index + 1
+
+
+
+
+ 
